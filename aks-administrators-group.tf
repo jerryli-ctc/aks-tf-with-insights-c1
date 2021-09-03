41d9067ce0499ca8888de4d9add13a9fad24fa8c
Jerry Li <jerry.li@cantire.com>
2021-09-03 13:02:11 -0400

    move azuread to main module

--- aks-administrators-group.tf
+++
@@ -1,4 +0,0 @@
-data "azuread_group" "aks_administrators" {
-  display_name        = "AKS-cluster-admins"
-  # import Kubernetes administrators for the AKS cluster
-}
--- main.tf
+++ main.tf
@@ -47,3 +47,9 @@ locals {
 
 resource "random_pet" "primary" {}
 
+data "azuread_group" "aks_administrators" {
+  display_name        = "AKS-cluster-admins"
+  # import Kubernetes administrators for the AKS cluster
+}
+
+
