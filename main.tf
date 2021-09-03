bbddb0a3c6c1628124afcd1e7befd2a3b5a84c50
Jerry Li <jerry.li@cantire.com>
2021-09-03 12:03:06 -0400

    final init

--- main.tf
+++ main.tf
@@ -23,12 +23,20 @@ terraform {
 }
 
 provider azurerm {
-  # v2.x required "features" block
+  # following variables required to login to azure. "az login from terrafrom cloud"
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   client_secret   = var.client_secret
   client_id       = var.client_id
+  # v2.x required "features" block
   features {}
+}
+
+provider "azuread" {
+  # client id and secrets of SPN required to read AAD and get AAD group info
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
 
 locals {
