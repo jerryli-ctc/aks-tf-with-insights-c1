ee35759688572e0424dfe5931dd6f2721b9eeff2
Jerry Li <jerry.li@cantire.com>
2021-09-03 12:06:48 -0400

    azuread_group name changed to display_name

--- aks-administrators-group.tf
+++ aks-administrators-group.tf
@@ -1,4 +1,4 @@
 resource "azuread_group" "aks_administrators" {
-  name        = "AKS-cluster-admins"
+  display_name        = "AKS-cluster-admins"
   description = "Kubernetes administrators for the ${local.aks_cluster_name} cluster."
 }
